da932991edd312c7397e1317ad33b9e70a7d1ec1
Mayowa Emmanuel Olabanji <mayowae@outlook.com>
2022-12-17 23:13:49 +0100

    Connect to DB

---
+++ src/database.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import { Pool } from "pg";
+
+dotenv.config();
+
+const {
+  PGHOST,
+  PGPORT,
+  PGUSER,
+  DBNAME,
+  PGPASSWORD
+} = process.env;
+
+const client = new Pool({
+  host: PGHOST,
+  port: PGPORT as (number|undefined),
+  user: PGUSER,
+  database: DBNAME,
+  password: PGPASSWORD,
+});
+
+export default client;
